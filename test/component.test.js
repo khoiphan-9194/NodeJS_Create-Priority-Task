2a10e992df31c4553836544363430bb270ad07df
Khoi Phan <jasonphan2631@gmail.com>
2024-09-16 20:46:51 -0700

    123

---
+++ test/component.test.js
@@ -0,0 +1,29 @@
+
+const Component = require('../lib/Component.js');
+
+
+describe('Component', () => {
+
+  
+    // describe('invalid', () => {
+    //   it('should return false for empty password', () => {
+    //     const str = '';
+  
+    //     const result = new Validate().isPassword(str);
+  
+    //     expect(result).toEqual(false);
+    //   });
+    // });
+
+
+    describe('Test whether or not render() throws an error', () => {
+      it('should throw an error', () => {
+        const component = new Component();
+  
+        expect(() => component.render()).toThrowError('Child class must implement render() method');
+      });
+    });
+
+
+  });
+  
